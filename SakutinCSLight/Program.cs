c371376d98d6856b5449cf1bfd13088d570bd422
Александр Жаворонок <C0baltt@live.com>
2023-04-27 22:49:41 +0300

    int playerHealth = 100;             int playerDamage = 3;             int enemyHealth = 100;             int enemyDamage = 3;

--- SakutinCSLight/Program.cs
+++ SakutinCSLight/Program.cs
@@ -6,21 +6,32 @@ namespace SakutinCSLight
     {
         static void Main(string[] args)
         {
-            int money;
-            int food;
-            int foodUnitPrice = 10;
-            bool isAbleToPay;
+            int playerHealth = 100;
+            int playerDamage = 3;
+            int enemyHealth = 100;
+            int enemyDamage = 3;
 
-            Console.WriteLine($"Добро пожаловать в пекарню! Сегодня еда по {foodUnitPrice} монет");
-            Console.Write("Сколько у вас золота?");
-            money = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Сколько еды вам нужно?");
-            food = Convert.ToInt32(Console.ReadLine());
+            while (playerHealth > 0 && enemyHealth > 0)
+            {
+                playerHealth -= enemyDamage;
+                enemyHealth -= playerDamage;
 
-            isAbleToPay = money >= food * foodUnitPrice;
-            food *= Convert.ToInt32(isAbleToPay);
-            money -= food * foodUnitPrice;
-            Console.WriteLine($"У вас в сумке {food} единиц еды, и {money} монет");
+                Console.WriteLine(playerHealth + " игрок.");
+                Console.WriteLine(enemyHealth + " враг.");
+            }
+            
+            if(playerHealth <= 0 && enemyHealth <= 0)
+            {
+                Console.WriteLine("Ничья.");
+            }
+            else if (enemyHealth <= 0)
+            {
+                Console.WriteLine("Победа игрока.");
+            }
+            else if (playerHealth <= 0)
+            {
+                Console.WriteLine("Победа врага.");
+            }
 
         }
     }
