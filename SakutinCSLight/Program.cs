76abf9d95802ba11b34fe4d3adbb2cf72ca002d3
Александр Жаворонок <C0baltt@live.com>
2023-06-01 21:22:21 +0300

    Связь Has-a. 6:34-6:40

--- SakutinCSLight/Program.cs
+++ SakutinCSLight/Program.cs
@@ -6,70 +6,60 @@ namespace SakutinCSLight
     {
         static void Main(string[] args)
         {
-            bool isOpen = true;
+            Performer worker1 = new Performer("Вениамин");
+            Performer worker2 = new Performer("Роман");
 
-            Table[] tables = { new Table(1, 4), new Table(2, 8), new Table(3, 10) };
+            Task[] tasks = { new Task(worker1, "Выкопать яму."), new Task(worker2, "Вывезти грунт.") };
 
-            while (isOpen)
+            Board schedule = new Board(tasks);
+
+            schedule.ShowAllTasks();
+        }
+    }
+
+    class Performer
+    {
+        public string Name;
+
+        public Performer(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Board
+    {
+        public Task[] Tasks;
+
+        public Board(Task[] tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public void ShowAllTasks()
+        {
+            for (int i = 0; i < Tasks.Length; i++)
             {
-                Console.WriteLine("Администрирование кафе.\n");
-
-                for (int i = 0; i < tables.Length; i++)
-                {
-                    tables[i].ShowInfo();
-                }
-
-                Console.Write("\nВведите номер стола: ");
-                int wishTable = Convert.ToInt32(Console.ReadLine()) - 1;
-                Console.Write("\nВведите количество мест для брони: ");
-                int desiredPlaces = Convert.ToInt32(Console.ReadLine());
-
-                bool isReservationCompleted = tables[wishTable].Reserve(desiredPlaces);
-
-                if (isReservationCompleted)
-                {
-                    Console.WriteLine("Бронь прошла успешно!");
-                }
-                else
-                {
-                    Console.WriteLine("Бронь не прошла. Недостаточно мест.");
-                }
-
-                Console.ReadKey();
-                Console.Clear();
+                Tasks[i].ShowInfo();
             }
         }
     }
 
-    class Table
+    class Task
     {
-        public int Number;
-        public int MaxPlaces;
-        public int FreePlaces;
+        public Performer Worker;
+        public string Description;
 
-        public Table(int number, int maxPlaces)
+        public Task(Performer worker, string description)
         {
-            Number = number;
-            MaxPlaces = maxPlaces;
-            FreePlaces = maxPlaces;
+            Worker = worker;
+            Description = description;
         }
 
         public void ShowInfo()
         {
-            Console.WriteLine($"Стол: {Number}. Свободно мест: {FreePlaces} из {MaxPlaces} .");
-        }
-
-        public bool Reserve(int places)
-        {
-            if (FreePlaces >= places)
-            {
-                FreePlaces -= places;
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            Console.WriteLine($"Ответственный: {Worker.Name}\nОписание задачи: {Description}.\n");
         }
     }
+
 }
