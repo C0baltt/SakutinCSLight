ad2caf48bc4593f7f167acd90089659334e1775f
Александр Жаворонок <C0baltt@live.com>
2023-06-01 20:17:45 +0300

    Практика ООП. Программа для администрирования кафе. 6:25-6:34

--- SakutinCSLight/Program.cs
+++ SakutinCSLight/Program.cs
@@ -1,143 +1,75 @@
 ﻿using System;
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SakutinCSLight
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
+            bool isOpen = true;
 
-            char[,] map = ReadMap("map.txt");
-            ConsoleKeyInfo pressedKey = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
+            Table[] tables = { new Table(1, 4), new Table(2, 8), new Table(3, 10) };
 
-            Task.Run(() =>
+            while (isOpen)
             {
-                while (true)
+                Console.WriteLine("Администрирование кафе.\n");
+
+                for (int i = 0; i < tables.Length; i++)
                 {
-                    pressedKey = Console.ReadKey();
+                    tables[i].ShowInfo();
                 }
-            });
 
-            int pacmanX = 1;
-            int pacmanY = 1;
-            int score = 0;
-            int steps = 0;
+                Console.Write("\nВведите номер стола: ");
+                int wishTable = Convert.ToInt32(Console.ReadLine()) - 1;
+                Console.Write("\nВведите количество мест для брони: ");
+                int desiredPlaces = Convert.ToInt32(Console.ReadLine());
 
-            while (true)
-            {
+                bool isReservationCompleted = tables[wishTable].Reserve(desiredPlaces);
+
+                if (isReservationCompleted)
+                {
+                    Console.WriteLine("Бронь прошла успешно!");
+                }
+                else
+                {
+                    Console.WriteLine("Бронь не прошла. Недостаточно мест.");
+                }
+
+                Console.ReadKey();
                 Console.Clear();
-
-                Console.ForegroundColor = ConsoleColor.Blue;
-                DrawMap(map);
-
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.SetCursorPosition(pacmanX, pacmanY);
-                Console.Write("@");
-
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.SetCursorPosition(32, 0);
-                Console.Write($"Score: {score}");
-
-                Console.SetCursorPosition(32, 1);
-                Console.Write($"Steps: {steps}");
-
-                pressedKey = Console.ReadKey();
-
-                HandleInput(pressedKey, ref pacmanX, ref pacmanY, map, ref score, ref steps);
-
-                Thread.Sleep(300);
             }
         }
+    }
 
-        private static char[,] ReadMap(string path)
+    class Table
+    {
+        public int Number;
+        public int MaxPlaces;
+        public int FreePlaces;
+
+        public Table(int number, int maxPlaces)
         {
-            string[] file = File.ReadAllLines("map.txt");
-
-            char[,] map = new char[GetMaxLenghtOfLine(file), file.Length];
-
-            for (int x = 0; x < map.GetLength(0); x++)
-                for (int y = 0; y < map.GetLength(1); y++)
-                    map[x, y] = file[y][x];
-
-            return map;
+            Number = number;
+            MaxPlaces = maxPlaces;
+            FreePlaces = maxPlaces;
         }
 
-        private static void DrawMap(char[,] map)
+        public void ShowInfo()
         {
-            for (int y = 0; y < map.GetLength(1); y++)
-            {
-                for (int x = 0; x < map.GetLength(0); x++)
-                {
-                    Console.Write(map[x, y]);
-                }
-                Console.Write("\n");
-            }
-
-
+            Console.WriteLine($"Стол: {Number}. Свободно мест: {FreePlaces} из {MaxPlaces} .");
         }
 
-        private static void HandleInput(ConsoleKeyInfo pressedKey, ref int pacmanX, ref int pacmanY, char[,] map, ref int score, ref int steps)
+        public bool Reserve(int places)
         {
-            int[] direction = GetDirection(pressedKey);
-
-            int nextPacmanPositionX = pacmanX + direction[0];
-            int nextPacmanPositionY = pacmanY + direction[1];
-
-            char nextCell = map[nextPacmanPositionX, nextPacmanPositionY];
-
-            if (nextCell == ' ' || nextCell == '.')
+            if (FreePlaces >= places)
             {
-                pacmanX = nextPacmanPositionX;
-                pacmanY = nextPacmanPositionY;
-
-                steps++;
-
-                if (nextCell == '.')
-                {
-                    score++;
-                    map[nextPacmanPositionX, nextPacmanPositionY] = ' ';
-                }
+                FreePlaces -= places;
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
-
-        private static int[] GetDirection(ConsoleKeyInfo pressedKey)
-        {
-            int[] direction = { 0, 0 };
-
-            if (pressedKey.Key == ConsoleKey.UpArrow)
-            {
-                direction[1] = -1;
-            }
-            else if (pressedKey.Key == ConsoleKey.DownArrow)
-            {
-                direction[1] = 1;
-            }
-            else if (pressedKey.Key == ConsoleKey.LeftArrow)
-            {
-                direction[0] = -1;
-            }
-            else if (pressedKey.Key == ConsoleKey.RightArrow)
-            {
-                direction[0] = 1;
-            }
-
-            return direction;
-        }
-
-        private static int GetMaxLenghtOfLine(string[] lines)
-        {
-            int maxLength = lines[0].Length;
-
-            foreach (var line in lines)
-                if (line.Length > maxLength)
-                    maxLength = line.Length;
-
-            return maxLength;
-        }
-
     }
 }
